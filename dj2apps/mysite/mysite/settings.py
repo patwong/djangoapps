c7db91909dff65eafb0552cd762f8a65bb2d5486
patwong <patrick.wong@uwaterloo.ca>
2018-08-31 13:17:41 -0700

    finish adding postgresql settings to tut

--- dj2apps/mysite/mysite/settings.py
+++ dj2apps/mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import psycopg2.extensions
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,12 +74,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# user: psqltest?
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgresql',
+        'USER': 'psqlnoob',
+        'PASSWORD': '123abc',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'client_encoding': 'UTF8',
+        'default_transaction_isolation': 'read committed'
     }
 }
+
 
 
 # Password validation
