73282bf02d0f8b06bc4b4d9c22351c9fa3a77499
patwong <patrick.wong@uwaterloo.ca>
2019-03-16 00:03:44 -0400

    change time zone settings to America/New_York

--- dj2apps/mysite/polls/models.py
+++ dj2apps/mysite/polls/models.py
@@ -1,9 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
+
+    def was_published_recently(self):
+        # returns True if the question was published within the last 24h
+        # rhs: subtracts one day from the the current UTC time
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    # end wpr()
+
+    def __str__(self):
+        return self.question_text
+    # end str()
 # end class
 
 
@@ -11,5 +24,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    # end str()
 # end Choice()
 
