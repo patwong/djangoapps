178eff23a25f84f30ea57ad29a565ce57b9837d9
patwong <patrick.wong@uwaterloo.ca>
2019-03-26 17:51:17 -0400

    adding basic latest question list view

--- dj2apps/mysite/polls/views.py
+++ dj2apps/mysite/polls/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
+
 # end index()
 
 
