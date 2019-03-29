0452370e48bba3b85ddc8b3791226e32500db525
patwong <patrick.wong@uwaterloo.ca>
2019-03-29 17:02:35 -0400

    adding render(), get404, and unambig URL

--- dj2apps/mysite/polls/views.py
+++ dj2apps/mysite/polls/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
@@ -11,16 +8,25 @@ def index(request):
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     # return HttpResponse("Hello, world. You're at the polls index.")
-
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list, }
+    return render(request, 'polls/index.html', context)
 
 # end index()
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # 1: basic 404 exception try/except block
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # end try/except
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    #  return statement #1
+    # return HttpResponse("You're looking at question %s." % question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 # end detail()
 
 
