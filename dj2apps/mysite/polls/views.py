06fe90a1f26c69df589222e082d80aa0ff613da9
patwong <patrick.wong@uwaterloo.ca>
2019-04-06 19:24:19 -0400

    adding F() for vote view

--- dj2apps/mysite/polls/views.py
+++ dj2apps/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.db.models import F
 from .models import Question, Choice
 
 
@@ -39,7 +40,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
