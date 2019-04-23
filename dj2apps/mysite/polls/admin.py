92a947b08ed0793c0e5236f4999475eba6abace5
patwong <patrick.wong@uwaterloo.ca>
2019-04-23 18:58:07 -0400

    starting part 7

--- dj2apps/mysite/polls/admin.py
+++ dj2apps/mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+# end class()
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+# end class()
+
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
