6c946f058e535372589f188489e3af80197f2322
patwong <patrick.wong@uwaterloo.ca>
2019-05-29 16:47:22 -0400

    adding heroku django demo

---
+++ herjango/hello/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+import requests
+import os
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+
+    # return render(request, "index.html")
+
+    # git demo
+#    r = requests.get('http://httpbin.org/status/418')
+#    print(r.text)
+#    return HttpResponse('<pre>' + r.text + '</pre>')
+
+    # part 11 config vars example
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
